7c08cfcdee5510adfa6295a14596bf577a9bdfb4
Ankush Agarwal <ankushagarwal11@gmail.com>
2020-06-28 20:59:50 -0700

    Update code

--- 2-Call-MsGraph-WithCertificate/confidential_client_certificate_sample.py
+++ 2-Call-MsGraph-WithCertificate/confidential_client_certificate_sample.py
@@ -8,12 +8,14 @@ import msal
 logging.basicConfig(level=logging.DEBUG)
 
 config = {
+  # tenant id
   "authority": "https://login.microsoftonline.com/0e764dbe-944c-4623-8539-52ba164e79bb",
-  "client_id": "74425ebe-eaf3-4448-b704-2643c2ae4822",
-  "scope": [ "74425ebe-eaf3-4448-b704-2643c2ae4822/.default" ],
-  "thumbprint": "4FE79A3F20CB21B3660005C9F3440655330BA093",
-  "private_key_file": "private_key.pem",
-  "mbs_endpoint": "https://aos.eastus.cloudapp.azure.com"
+  "client_id": "d8416169-b121-4ca3-ab61-2b92659f2ec8",
+  "scope": [ "d8416169-b121-4ca3-ab61-2b92659f2ec8/.default" ],
+  # openssl x509 -in public_key_certificate.crt -fingerprint -noout | sed 's|:||g'
+  "thumbprint": "F5F55423D518CC0808B53A624042259D0083E07E",
+  "private_key_file": "/Users/kush/tmp/2020-06-15-16-53-44/private_key.pem",
+  "mbs_endpoint": "http://localhost:8080"
 }
 
 # Create a preferably long-lived app instance which maintains a token cache.
@@ -37,11 +39,12 @@ if not token:
 
 print(token)
 
-# preapproval_payload = {
-#   'foo': 'bar'
-# }
-# headers = {
-#   'Authorization': 'Bearer ' + token['access_token']
-# }
-# response = requests.post(config["mbs_endpoint"] + "/api/v1/preapproval", json=preapproval_payload)
-# print(response)
+preapproval_payload = {
+  'currency': 'Coin1',
+  'amount': 100000
+}
+headers = {
+  'Authorization': 'Bearer ' + token['access_token']
+}
+response = requests.post(config["mbs_endpoint"] + "/api/v1/mint/pre_approval", headers=headers, json=preapproval_payload)
+print(response)
